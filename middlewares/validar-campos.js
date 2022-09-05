1f99e8c0ce41152d2d7fde16390a507730d33f4b
AndresCamiloChavez <andres.cz.chavez@gmail.com>
2022-09-05 17:15:20 -0500

    función que válida los campos del bode (example)

--- middlewares/validar-campos.js
+++ middlewares/validar-campos.js
@@ -4,6 +4,7 @@ const validarCampos = (req, res, next) => {
 
     const errores = validationResult(req);
 
+    // valida que los campos existan
     if(!errores.isEmpty()){
         return res.status(400).json({
             ok: false,
